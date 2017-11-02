149c0ba71c5fe528334ea6dfb66c9e5c59a5c13e
cusmaimatteo@gmail.com <London.2012>
2017-11-02 17:48:09 +0100

    non capisco dove chiami openmaint

--- src/main/java/it/storelink/openmaintmango/AllineatoreProcessor.java
+++ src/main/java/it/storelink/openmaintmango/AllineatoreProcessor.java
@@ -1,28 +1,30 @@
 package it.storelink.openmaintmango;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
 import it.storelink.mango.api.DefaultApiImpl;
 import it.storelink.mango.api.MangoRestApi;
 import it.storelink.mango.model.PointValueModel;
 import it.storelink.openmaintmango.config.ConfigSingleton;
 import it.storelink.openmaintmango.openmaint.client.building.Output;
 import it.storelink.openmaintmango.xmlconfig.SensoreType;
-import org.glassfish.jersey.client.ClientConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 import java.util.Iterator;
 import java.util.List;
 
 
 public class AllineatoreProcessor {
+
     private static Logger logger = LoggerFactory.getLogger(AllineatoreProcessor.class);
 
     private static MangoRestApi api;
     private static Boolean initialized = false;
 
-    private ClientConfig clientConfig = new ClientConfig();
-    private Client client = ClientBuilder.newClient(clientConfig);
-    private WebTarget webTarget = client.target(getOpenMaintBaseURI());
+    //private Client client = Client.create();
+    //private WebResource webTarget = client.resource(getOpenMaintBaseURI());
 
 
     List<SensoreType> listaSensori = null;
@@ -51,43 +53,19 @@ public class AllineatoreProcessor {
         }
     }
 
+    /*
     public static void fra2(String[] args) {
         ClientConfig clientConfig = new ClientConfig();
-        // clientConfig.register(MyClientResponseFilter.class);
-        // clientConfig.register(new AnotherClientFilter());
 
         Client client = ClientBuilder.newClient(clientConfig);
-        //client.register(ThirdClientFilter.class);
 
         WebTarget webTarget = client.target(getOpenMaintBaseURI());
-        // webTarget.register(FilterForExampleCom.class);
         WebTarget sessionsWebTarget = webTarget.path("/sessions/");
-        //  WebTarget helloworldWebTarget = resourceWebTarget;
-        //   WebTarget helloworldWebTarget = resourceWebTarget.path("helloworld");
-        //   WebTarget helloworldWebTargetWithQueryParam =
-        //           helloworldWebTarget.queryParam("greeting", "Hi World!");
-
-        //  Invocation.Builder invocationBuilder =
-        //   helloworldWebTarget.request(MediaType.APPLICATION_JSON);
-        //   invocationBuilder.header("Content-Type", "application/json");
-
-        // Response response = invocationBuilder.get();
-
-        /*String response = webTarget.path("sessions").
-                request().
-                accept(MediaType.APPLICATION_JSON_TYPE).
-                get(Response.class)
-                .toString();*/
         User user = new User("admin", "pIPP0");
 
         Response response = sessionsWebTarget.request().post(Entity.entity(user, MediaType.APPLICATION_JSON));
-
-        //System.out.println(response);
-        //System.out.println(response.getStatus());
-        // System.out.println(response.getStatusInfo());
         it.storelink.openmaintmango.openmaint.client.sessions.Output sessions = response.readEntity(it.storelink.openmaintmango.openmaint.client.sessions.Output.class);
         System.out.println(sessions.data._id);
-
 
         WebTarget buildingWebTarget = webTarget.path("/classes/Building/attributes/");
 
@@ -102,6 +80,7 @@ public class AllineatoreProcessor {
         Output building = responseGet.readEntity(Output.class);
         System.out.println(building.meta.total);
     }
+    */
 
     private static URI getOpenMaintBaseURI() {
         return UriBuilder.fromUri(ConfigSingleton.getInstance().getSystemParam_OPENMAINT_BASE_URL()).build();
